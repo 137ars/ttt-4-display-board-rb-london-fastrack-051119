46c6a83355da430a59170da36376fc47ca53c3d9
Alec Simpson <alecrsimpson@gmail.com>
2019-05-12 12:05:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,3 +6,9 @@ puts "-----------"
 puts " #{board[3]} "  "|" " #{board[4]} "  "|" " #{board[5]} "
 puts "-----------"
 puts " #{board[6]} " "|"  " #{board[7]} " "|"  " #{board[8]} "
+
+def display_board(board)
+  puts "(board)"
+end
+
+display_board
